086528f20b5704121bc42e48965469c11ffc220a
inhadeepblue <deepblue@inha.ac.kr>
2025-12-31 09:54:17 +0900

    week07 : polymorphism vii

---
+++ week07/app01.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class UndergraduateStudent {
+public:
+	void warn() {
+		cout << "학사경고\n";
+	}
+};
+class DormitoryStudent {
+public:
+	void warn() {
+		cout << "벌점부여\n";
+	}
+};
+class UndergraduateDormitoryStudent : public UndergraduateStudent, public DormitoryStudent {
+
+};
+int main() {
+	UndergraduateDormitoryStudent uds;
+	//uds.warn();  // ambiguous
+	uds.DormitoryStudent::warn();
+
+	return 0;
+}
