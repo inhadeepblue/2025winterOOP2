a06d93ecdf470e99e109248bc034281501b7607d
inhadeepblue <deepblue@inha.ac.kr>
2025-12-31 11:05:33 +0900

    week07 : operator overloading ii

--- week07/app02.cpp
+++ week07/app02.cpp
@@ -10,15 +10,22 @@ public:
     Complex(int real, int imaginary) : real(real), imaginary(imaginary) {
     }
 
-    Complex operator+(Complex right) {
-        return Complex(this->real + right.real, this->imaginary + right.imaginary);
-    }
+    //Complex operator+(Complex right) {
+    //    return Complex(this->real + right.real, this->imaginary + right.imaginary);
+    //}
 };
+
+// Overloading as a nonmember
+Complex operator+(Complex left, Complex right) {
+    return Complex(left.real + right.real, left.imaginary + right.imaginary);
+}
+
 int main()
 {
     Complex c1(9, 7);
     Complex c2(1, 4);
-    Complex c3 = c1.operator+(c2);  //Complex c3 = c1 + c2;
+    //Complex c3 = c1.operator+(c2);
+    Complex c3 = operator+(c1, c2);  //Complex c3 = c1 + c2;
 
     cout << c3.real << "+" << c3.imaginary << "i\n";
 
