26f52536fa8a8f9ce284dd8c503fea8b9db05986
inhadeepblue <deepblue@inha.ac.kr>
2025-12-31 10:59:06 +0900

    week07 : operator overloading i

---
+++ week07/app02.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+class Complex {
+public:
+    int real;
+    int imaginary;
+
+    Complex(int real, int imaginary) : real(real), imaginary(imaginary) {
+    }
+
+    Complex operator+(Complex right) {
+        return Complex(this->real + right.real, this->imaginary + right.imaginary);
+    }
+};
+int main()
+{
+    Complex c1(9, 7);
+    Complex c2(1, 4);
+    Complex c3 = c1.operator+(c2);  //Complex c3 = c1 + c2;
+
+    cout << c3.real << "+" << c3.imaginary << "i\n";
+
+    return 0;
+}
