035a3061a0a107334f0f64f1a05b727cb39f6630
inhadeepblue <deepblue@inha.ac.kr>
2026-01-06 11:00:59 +0900

    week10 : exception handling iii

--- week10/app01.cpp
+++ week10/app01.cpp
@@ -6,6 +6,8 @@ int main()
 
     da1.setAt(100, 3);
     cout << da1.getAt(3) << '\n';
+    cout << da1.getAt(9) << '\n';  // out of heap memory scope
+    da1.setAt(-99, 7);  // out of heap memory scope
 
     return 0;
 }
