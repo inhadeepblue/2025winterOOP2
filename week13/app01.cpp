28bc324d87375c7324e537bfa5985371cd8b9e93
inhadeepblue <deepblue@inha.ac.kr>
2026-01-12 10:35:18 +0900

    week13 : stl i

---
+++ week13/app01.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <iostream>
+#include <iomanip>  // setw
+using namespace std;
+
+int main()
+{
+    //vector<int> vec(10);
+    vector<int> vec;
+
+    for (int i = 0; i < 10; i++)
+    {
+        //vec.at(i) = i * 2;
+        //vec[i] = i * 2;
+        vec.push_back(i * 2);
+        //vec.push_front(i * 2);  // not exist
+    }
+    
+    for (vector<int>::iterator iter = vec.begin(); iter != vec.end(); ++iter)
+    {
+        cout << setw(3) << *iter;
+    }
+    cout << endl;
+
+    for (vector<int>::reverse_iterator rIter = vec.rbegin(); rIter != vec.rend(); ++rIter)
+    {
+        cout << setw(3) << *rIter;
+    }
+    cout << endl;
+    return 0;
+}
